05937c16f2e644ac127154634a9aa5b234e1b065
Marko Matusovic <matusovic.m@gmail.com>
2022-12-04 07:06:41 +0100

    Change the template

--- 2022/src/solutions/day_template.rs
+++ 2022/src/solutions/day_template.rs
@@ -1,26 +1,26 @@
-pub fn day() -> u8 { xxx }
+pub fn day() -> u8 {
+    xxx
+}
 
 #[derive(Debug)]
 pub struct Input {
-    lines: Vec<String>
+    lines: Vec<String>,
 }
 
 pub fn parse_input(raw: &str) -> Input {
-    let lines:Vec<&str> = raw.split("\n").to_owned.collect();
+    let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
     println!("Day {} parsing {} lines", day(), lines.len());
-    Input { 
-        lines: lines
-    }
+    Input { lines: lines }
 }
 
-pub fn part_1(input: &Input){
+pub fn part_1(input: &Input) {
     println!("Day {} part 1", day());
 
     println!("Answer is {}", 0);
 }
 
-pub fn part_2(input: &Input){
+pub fn part_2(input: &Input) {
     println!("Day {} part 2", day());
-    
+
     println!("Answer is {}", 0);
 }
