efa1453a930b3fd8f9c4be48bafe5185a98838ff
Marko Matušovič <matusovic.m@gmail.com>
2023-12-02 12:09:57 +0100

    Add day 2

--- 2023/src/solutions/day_02.rs
+++ 2023/src/solutions/day_02.rs
@@ -1,13 +1,57 @@
+use std::{hint::black_box, cmp::max};
+
 use super::day_trait::Day;
 
-#[derive(Debug)]
-pub struct Input {
-    lines: Vec<String>,
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Draw {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Game {
+    id: u32,
+    draws: Vec<Draw>,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Input {
+    games: Vec<Game>,
 }
 
 fn parse_input(raw: &str) -> Input {
     let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
-    Input { lines: lines }
+    Input {
+        games: lines
+            .iter()
+            .map(|l| {
+                let (id, draws) = l[5..].split_once(": ").expect("Invalid input!");
+                Game {
+                    id: id.parse().expect("Cannot parse id!"),
+                    draws: draws
+                        .split("; ")
+                        .into_iter()
+                        .map(|r| {
+                            let mut draw = Draw {
+                                red: 0,
+                                green: 0,
+                                blue: 0,
+                            };
+                            r.split(", ").for_each(|d| match d.split_once(" ") {
+                                Some((n, "red")) => draw.red = n.parse().unwrap(),
+                                Some((n, "green")) => draw.green = n.parse().unwrap(),
+                                Some((n, "blue")) => draw.blue = n.parse().unwrap(),
+                                _ => panic!(),
+                            });
+
+                            return draw;
+                        })
+                        .collect(),
+                }
+            })
+            .collect(),
+    }
 }
 
 pub struct Day02;
@@ -18,15 +62,42 @@ impl Day for Day02 {
 
     fn part_1(&self, raw: &str) {
         println!("Day {} part 1", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let sum: u32 = input
+            .games
+            .iter()
+            .filter(|&g| {
+                g.draws
+                    .iter()
+                    .all(|d| d.red <= 12 && d.green <= 13 && d.blue <= 14)
+            })
+            .map(|g| g.id)
+            .sum();
+
+        println!("Answer is {}", sum);
     }
 
     fn part_2(&self, raw: &str) {
         println!("Day {} part 2", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let sum: u32 = input.games.iter().map(|g| {
+            let mut max_draw = Draw {
+                red: 0,
+                green: 0,
+                blue: 0,
+            };
+            
+            g.draws.iter().for_each(|round| {
+                max_draw.red = max(max_draw.red, round.red);
+                max_draw.green = max(max_draw.green, round.green);
+                max_draw.blue = max(max_draw.blue, round.blue);
+            });
+            
+            max_draw.red * max_draw.green * max_draw.blue
+        }).sum();
+
+        println!("Answer is {}", sum);
     }
 }
