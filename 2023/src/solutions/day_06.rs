428a7e28f6212cfef82227a60cbc7a51cc1b5f75
Marko Matušovič <matusovic.m@gmail.com>
2023-12-06 10:20:01 +0100

    Add day 6

--- 2023/src/solutions/day_06.rs
+++ 2023/src/solutions/day_06.rs
@@ -1,13 +1,35 @@
+use itertools::Itertools;
+
 use super::day_trait::Day;
 
 #[derive(Debug)]
 pub struct Input {
     lines: Vec<String>,
+    races: Vec<(usize, usize)>,
 }
 
 fn parse_input(raw: &str) -> Input {
     let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
-    Input { lines: lines }
+    let races = lines
+        .iter()
+        .map(|l| {
+            l.split(' ')
+                .skip(1)
+                .filter(|ch| !ch.is_empty())
+                .map(|ch| ch.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect_vec()
+        .chunks(2)
+        .into_iter()
+        .map(|slice| match slice {
+            [t, d] => t.iter().map(|&t| t).zip(d.to_owned()).collect_vec(),
+            _ => panic!(),
+        })
+        .find(|_| true)
+        .unwrap();
+
+    Input { lines, races }
 }
 
 pub struct Day06;
@@ -18,15 +40,50 @@ impl Day for Day06 {
 
     fn part_1(&self, raw: &str) {
         println!("Day {} part 1", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let prd: usize = input
+            .races
+            .iter()
+            .map(|&(t, d)| {
+                let tf: f64 = t as f64;
+                let df: f64 = d as f64;
+
+                let x1 = ((tf - (tf * tf - 4.0 * df).sqrt()) / 2.0).ceil() as usize;
+                let x2 = ((tf + (tf * tf - 4.0 * df).sqrt()) / 2.0).floor() as usize;
+
+                return x2 - x1 + 1;
+            })
+            .product();
+
+        println!("Answer is {}", prd);
     }
 
     fn part_2(&self, raw: &str) {
         println!("Day {} part 2", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let vals: Vec<usize> = input
+            .lines
+            .iter()
+            .map(|l| {
+                l.split_once(": ")
+                    .unwrap()
+                    .1
+                    .trim()
+                    .parse::<usize>()
+                    .unwrap()
+            })
+            .collect();
+
+        let time = vals.get(0).unwrap().to_owned() as f64;
+        let dist = vals.get(1).unwrap().to_owned() as f64;
+
+        let x1 = ((time - (time * time - 4.0 * dist).sqrt()) / 2.0).ceil() as usize;
+        let x2 = ((time + (time * time - 4.0 * dist).sqrt()) / 2.0).floor() as usize;
+
+        let res = x2 - x1 + 1;
+
+        println!("Answer is {}", res);
     }
 }
