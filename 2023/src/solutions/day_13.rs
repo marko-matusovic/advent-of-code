33480b7191933b97eca5d93b783c34ca7cbb2f3d
Marko Matušovič <matusovic.m@gmail.com>
2023-12-13 09:32:47 +0100

    WIP day 13

--- 2023/src/solutions/day_13.rs
+++ 2023/src/solutions/day_13.rs
@@ -3,11 +3,43 @@ use super::day_trait::Day;
 #[derive(Debug)]
 pub struct Input {
     lines: Vec<String>,
+    maps: Vec<Vec<u128>>,
 }
 
 fn parse_input(raw: &str) -> Input {
     let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
-    Input { lines }
+
+    let maps: Vec<Vec<u128>> = raw
+        .split("\n\n")
+        .map(|block| {
+            block
+                .split("\n")
+                .map(|l| {
+                    let mut n = 0;
+
+                    l.char_indices().for_each(|(i, ch)| {
+                        if ch == '#' {
+                            n += 1 << i;
+                        }
+                    });
+
+                    n
+                })
+                .collect()
+        })
+        .collect();
+
+    Input { lines, maps }
+}
+
+fn find_symetry(matrix: &Vec<u128>) -> Option<usize> {
+
+    
+    todo!()
+}
+
+fn t(matrix: &Vec<u128>) -> Vec<u128> {
+    todo!()
 }
 
 pub struct Day13;
@@ -18,9 +50,15 @@ impl Day for Day13 {
 
     fn part_1(&self, raw: &str) {
         println!("Day {} part 1", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let sum: usize = input
+            .maps
+            .iter()
+            .map(|mx| find_symetry(mx).unwrap_or_else(|| 100 * find_symetry(&t(mx)).unwrap()))
+            .sum();
+
+        println!("Answer is {}", sum);
     }
 
     fn part_2(&self, raw: &str) {
