72285cb9a0bcb81bc33e5a317d1496c41e975709
Marko Matušovič <marko.matusovic@teampicnic.com>
2024-12-07 20:24:54 +0100

    Add day 1

--- rust/2024/src/solutions/day_01.rs
+++ rust/2024/src/solutions/day_01.rs
@@ -1,13 +1,27 @@
+use std::iter::zip;
+
+use itertools::Itertools;
+use pathfinding::num_traits::abs;
+
 use super::day_trait::Day;
 
 #[derive(Debug)]
 pub struct Input {
     lines: Vec<String>,
+    left: Vec<isize>,
+    right: Vec<isize>,
 }
 
 fn parse_input(raw: &str) -> Input {
     let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
-    Input { lines }
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+    for line in &lines {
+        let (a, b) = line.split_once("   ").unwrap();
+        left.push(a.parse().unwrap());
+        right.push(b.parse().unwrap());
+    }
+    Input { lines, left, right }
 }
 
 pub struct Day01;
@@ -18,15 +32,25 @@ impl Day for Day01 {
 
     fn part_1(&self, raw: &str) {
         println!("Day {} part 1", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let sum: isize = zip(input.left.iter().sorted(), input.right.iter().sorted())
+            .map(|(a, b)| abs(a - b))
+            .sum();
+
+        println!("Answer is {}", sum);
     }
 
     fn part_2(&self, raw: &str) {
         println!("Day {} part 2", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let sum: isize = input
+            .left
+            .iter()
+            .map(|l| input.right.iter().filter(|&r| r == l).count() as isize * l)
+            .sum();
+
+        println!("Answer is {}", sum);
     }
 }
