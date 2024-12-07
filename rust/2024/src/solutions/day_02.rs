9217ff021d27dc4a86e7a5d50e95825ff245248f
Marko Matušovič <marko.matusovic@teampicnic.com>
2024-12-07 20:52:22 +0100

    Add day 2

--- rust/2024/src/solutions/day_02.rs
+++ rust/2024/src/solutions/day_02.rs
@@ -1,13 +1,22 @@
+use pathfinding::num_traits::abs;
+
 use super::day_trait::Day;
+
+type Report = Vec<usize>;
 
 #[derive(Debug)]
 pub struct Input {
     lines: Vec<String>,
+    reports: Vec<Report>,
 }
 
 fn parse_input(raw: &str) -> Input {
     let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
-    Input { lines }
+    let reports = lines
+        .iter()
+        .map(|l| l.split(" ").map(|d| d.parse().unwrap()).collect())
+        .collect();
+    Input { lines, reports }
 }
 
 pub struct Day02;
@@ -18,15 +27,36 @@ impl Day for Day02 {
 
     fn part_1(&self, raw: &str) {
         println!("Day {} part 1", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let count: usize = input.reports.iter().filter(|&r| safe(r)).count();
+
+        println!("Answer is {}", count);
     }
 
     fn part_2(&self, raw: &str) {
         println!("Day {} part 2", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let count: usize = input
+            .reports
+            .iter()
+            .filter(|&r| {
+                (0..r.len()).any(|i| {
+                    let mut q = r.clone();
+                    q.remove(i);
+                    safe(&q)
+                })
+            })
+            .count();
+
+        println!("Answer is {}", count);
     }
+}
+
+fn safe(report: &Report) -> bool {
+    let increasing = report[0] < report[1];
+    report.windows(2).all(|w| {
+        w[0] != w[1] && abs(w[0] as f32 - w[1] as f32) <= 3. && (w[0] < w[1]) == increasing
+    })
 }
