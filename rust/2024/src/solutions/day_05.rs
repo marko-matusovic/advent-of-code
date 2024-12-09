0f7b69b2212f18e44db3c87e2ced559ad68ea4b1
Marko Matušovič <marko.matusovic@teampicnic.com>
2024-12-09 22:08:26 +0100

    Add day 5

--- rust/2024/src/solutions/day_05.rs
+++ rust/2024/src/solutions/day_05.rs
@@ -1,13 +1,35 @@
+use itertools::Itertools;
+
 use super::day_trait::Day;
 
 #[derive(Debug)]
 pub struct Input {
     lines: Vec<String>,
+    rules: Vec<(usize, usize)>,
+    books: Vec<Vec<usize>>,
 }
 
 fn parse_input(raw: &str) -> Input {
     let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
-    Input { lines }
+    let (raw_rules, raw_books) = raw.split_once("\n\n").unwrap();
+    let rules = raw_rules
+        .split("\n")
+        .map(|r| {
+            r.split_once("|")
+                .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap()))
+                .unwrap()
+        })
+        .collect_vec();
+    let books = raw_books
+        .split("\n")
+        .map(|b| b.split(",").map(|n| n.parse().unwrap()).collect_vec())
+        .collect_vec();
+
+    Input {
+        lines,
+        rules,
+        books,
+    }
 }
 
 pub struct Day05;
@@ -18,15 +40,63 @@ impl Day for Day05 {
 
     fn part_1(&self, raw: &str) {
         println!("Day {} part 1", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let sum: usize = input
+            .books
+            .iter()
+            .filter(|&book| {
+                input.rules.iter().all(|rule| {
+                    if let (Some(p1), Some(p2)) = (
+                        book.iter().position(|&n| n == rule.0),
+                        book.iter().position(|&n| n == rule.1),
+                    ) {
+                        p1 < p2
+                    } else {
+                        true
+                    }
+                })
+            })
+            .map(|book| book[book.len() / 2])
+            .sum();
+
+        println!("Answer is {}", sum);
     }
 
     fn part_2(&self, raw: &str) {
         println!("Day {} part 2", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let sum: usize = input
+            .books
+            .iter()
+            .filter_map(|book| {
+                let mut corrected_book = book.clone();
+                let mut corrected = false;
+                let mut modified = true;
+                while modified {
+                    modified = false;
+                    for rule in &input.rules {
+                        if let (Some(p1), Some(p2)) = (
+                            corrected_book.iter().position(|&n| n == rule.0),
+                            corrected_book.iter().position(|&n| n == rule.1),
+                        ) {
+                            if p1 > p2 {
+                                corrected = true;
+                                modified = true;
+                                (corrected_book[p1], corrected_book[p2]) =
+                                    (corrected_book[p2], corrected_book[p1])
+                            }
+                        }
+                    }
+                }
+                match corrected {
+                    true => Some(corrected_book[corrected_book.len() / 2]),
+                    false => None,
+                }
+            })
+            .sum();
+
+        println!("Answer is {}", sum);
     }
 }
