c43fdc49d8306a65b5d7120a805cf31e6f314faa
Marko Matušovič <matusovic.m@gmail.com>
2025-12-07 11:02:37 +0100

    Add day 7

--- rust/2025/src/solutions/day_07.rs
+++ rust/2025/src/solutions/day_07.rs
@@ -1,13 +1,30 @@
+use std::collections::HashSet;
+
+use cached::proc_macro::cached;
+
 use super::day_trait::Day;
 
 #[derive(Debug)]
 pub struct Input {
-    lines: Vec<String>,
+    start: usize,
+    splitters: Vec<Vec<usize>>,
 }
 
 fn parse_input(raw: &str) -> Input {
-    let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
-    Input { lines }
+    Input {
+        start: raw.split_once("\n").unwrap().0.find("S").unwrap(),
+        splitters: raw
+            .lines()
+            .skip(1)
+            .map(|line| {
+                line.chars()
+                    .enumerate()
+                    .filter(|(_, ch)| *ch == '^')
+                    .map(|(i, _)| i)
+                    .collect()
+            })
+            .collect(),
+    }
 }
 
 pub struct Day07;
@@ -18,15 +35,50 @@ impl Day for Day07 {
 
     fn part_1(&self, raw: &str) {
         println!("Day {} part 1", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let mut beams = HashSet::new();
+        beams.insert(input.start);
+        let mut total_splits = 0;
+
+        for splitters in input.splitters {
+            let mut new_beams = HashSet::new();
+            for beam in beams {
+                if splitters.contains(&beam) {
+                    total_splits += 1;
+                    new_beams.insert(beam - 1);
+                    new_beams.insert(beam + 1);
+                } else {
+                    new_beams.insert(beam);
+                }
+            }
+            beams = new_beams;
+        }
+
+        println!("Answer is {}", total_splits);
     }
 
     fn part_2(&self, raw: &str) {
         println!("Day {} part 2", self.day());
-        let _input: Input = parse_input(raw);
+        let input: Input = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let count = count_timelines(input.start, input.splitters.clone());
+
+        println!("Answer is {}", count);
+    }
+}
+
+#[cached]
+fn count_timelines(beam: usize, splitters: Vec<Vec<usize>>) -> usize {
+    if let Some(current_splitters) = splitters.first() {
+        let remaining_splitters = splitters[1..].to_vec();
+        if current_splitters.contains(&beam) {
+            count_timelines(beam - 1, remaining_splitters.clone())
+                + count_timelines(beam + 1, remaining_splitters.clone())
+        } else {
+            count_timelines(beam, remaining_splitters.clone())
+        }
+    } else {
+        1
     }
 }
