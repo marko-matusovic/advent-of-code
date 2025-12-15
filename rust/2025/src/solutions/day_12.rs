803cf5a67df55e554b5900e256e772ef09ade507
Marko Matušovič <marko.matusovic@teampicnic.com>
2025-12-15 10:03:27 +0100

    Add day 12

--- rust/2025/src/solutions/day_12.rs
+++ rust/2025/src/solutions/day_12.rs
@@ -1,13 +1,55 @@
 use super::day_trait::Day;
+use itertools::Itertools;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Input {
-    lines: Vec<String>,
+    shapes: HashMap<usize, Vec<Vec<bool>>>,
+    trees: Vec<((usize, usize), HashMap<usize, usize>)>,
 }
 
 fn parse_input(raw: &str) -> Input {
-    let lines: Vec<String> = raw.split("\n").map(|s| s.to_owned()).collect();
-    Input { lines }
+    Input {
+        shapes: raw
+            .split("\n\n")
+            .map_while(|raw_shape| {
+                if let Some((i, raw_shape)) = raw_shape.split_once(":\n") {
+                    Some((
+                        i.parse().unwrap(),
+                        raw_shape
+                            .lines()
+                            .map(|line| line.chars().map(|c| c == '#').collect())
+                            .collect(),
+                    ))
+                } else {
+                    None
+                }
+            })
+            .collect(),
+        trees: raw
+            .split("\n\n")
+            .tail(1)
+            .next()
+            .unwrap()
+            .lines()
+            .map(|line| {
+                let (dims, counts) = line.split_once(": ").unwrap();
+                let (x, y) = dims
+                    .split("x")
+                    .map(|n| n.parse().unwrap())
+                    .collect_tuple()
+                    .unwrap();
+                (
+                    (x, y),
+                    counts
+                        .split(' ')
+                        .map(|n| n.parse().unwrap())
+                        .enumerate()
+                        .collect(),
+                )
+            })
+            .collect(),
+    }
 }
 
 pub struct Day12;
@@ -18,15 +60,30 @@ impl Day for Day12 {
 
     fn part_1(&self, raw: &str) {
         println!("Day {} part 1", self.day());
-        let _input: Input = parse_input(raw);
+        let Input { shapes, trees } = parse_input(raw);
 
-        println!("Answer is {}", 0);
+        let shape_size: HashMap<usize, usize> = shapes
+            .iter()
+            .map(|(i, shape)| {
+                (
+                    i.clone(),
+                    shape
+                        .iter()
+                        .map(|row| row.iter().filter(|x| **x).count())
+                        .sum(),
+                )
+            })
+            .collect();
+
+        let fits_by_size = trees
+            .iter()
+            .filter(|((x, y), counts)| {
+                counts.iter().map(|(i, c)| c * shape_size[i]).sum::<usize>() <= (x * y)
+            })
+            .count();
+
+        println!("Answer is {}", fits_by_size);
     }
 
-    fn part_2(&self, raw: &str) {
-        println!("Day {} part 2", self.day());
-        let _input: Input = parse_input(raw);
-
-        println!("Answer is {}", 0);
-    }
+    fn part_2(&self, _raw: &str) {}
 }
